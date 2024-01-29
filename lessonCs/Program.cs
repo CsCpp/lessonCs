eb552666a37d2dd27799b07f1d61814cf91dfd96
CppKotlin <cscppkotlin@gmail.com>
2024-01-29 22:41:43 +0300

    //Рисуем треугольnики

--- lessonCs/Program.cs
+++ lessonCs/Program.cs
@@ -1,7 +1,8 @@
 ﻿using System;
+using System.Drawing;
 
-                            //Операторв
-                            // Арифметические операции с числами
+//Рисуем треугольnики
+
 
 
 namespace LessonCs
@@ -10,15 +11,73 @@ namespace LessonCs
     {
         static void Main(string[] args)
         {
-            int a = 10;
-            int b = 3;
-             double result = (double)a / b;
-            int c = a % b;    
 
-            Console.WriteLine("Частное a/b=" + result);
-            Console.WriteLine("Остаток от деления = "+c);
+           Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Введите высоту треугольника :");
+            int l = int.Parse(Console.ReadLine());
+            Console.WriteLine("-------------------------------------------------------");
+            Console.WriteLine("-------------------------------------------------------");
+            for (int i = 0; i < l; i++)
+            {
+                for (int ai=0;ai<=i; ai++) 
+                { Console.Write("*");
 
-            Console.ReadLine();
+                }    
+                Console.WriteLine();
+            }
+            Console.WriteLine("-------------------------------------------------------");
+            Console.WriteLine("-------------------------------------------------------");
+            for (int i = l; i > 0; i--)
+            {
+                for (int ai = 0; ai < i; ai++)
+                {
+                    Console.Write("*");
+
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("-------------------------------------------------------");
+            Console.WriteLine("-------------------------------------------------------");
+            for (int i = 0; i < l; i++)
+            {
+                for (int ai = l; ai > i; ai--)
+                {
+                    Console.Write("*");
+
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("-------------------------------------------------------");
+            Console.WriteLine("-------------------------------------------------------");
+            for (int i = 0; i < l; i++)
+            {
+                for (int ai = l-1; ai > i; ai--)
+                {
+                    Console.Write(" ");
+
+                }
+                for (int ai = 0; ai <= i; ai++)
+                {
+                    Console.Write("*");
+
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("-------------------------------------------------------");
+            Console.WriteLine("-------------------------------------------------------");
+            
+          /*  for (int i = 0; i < l; i++)
+            {
+                Console.SetCursorPosition(l-i,i+1);
+                for (int ai = 0; ai <= i; ai++)
+                {
+                    Console.Write("*");
+
+                }
+                Console.WriteLine();
+
+            }*/
         }
+
     }
 }
