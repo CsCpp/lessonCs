b49c2739813f4aeb029397e677705e6058a2a4db
CppKotlin <cscppkotlin@gmail.com>
2024-02-04 21:35:07 +0300

    //оператор null-объединения ??

--- lessonCs/Program.cs
+++ lessonCs/Program.cs
@@ -4,25 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-//Ссылочные (referens types) и значимые (valye types) типы
-// Стек (stack) и куча (heap)
+//оператор null-объединения ??
+
 
 namespace Lesson
 {
     class Program
     {
-        static void Foo(int x) { x = 3; }
-        static void Foo(int[] x) { x[0] = 3; }
         static void Main(string[] args)
         {
-            Console.ForegroundColor = ConsoleColor.Cyan;
-            int x = 4; // stack
-            Foo(x);
-            Console.WriteLine(x);
-            int[] arr = { 4 }; //heap
-            Foo(arr); 
-            Console.WriteLine(arr[0]);
-                     
+            Console.ForegroundColor = ConsoleColor.Green;
+           
+            string str = null;
+            if(str == null) { Console.WriteLine("Null");}
+
+            Console.WriteLine(str ?? "Нету данных");
+
+            string result=str ?? string.Empty;
+            Console.WriteLine("Длинна строки = "+result.Length);
+            
+            Console.ReadLine();
         }
 
     }
