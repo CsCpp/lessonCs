d1877a8f86359f4df00badb4aa3674268f9056a3
CppKotlin <cscppkotlin@gmail.com>
2024-02-06 23:16:43 +0300

    //оператор условного null ?.

--- lessonCs/Program.cs
+++ lessonCs/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-//оператор присваивания объединения со значением null ??=
+//оператор условного null ?.
 
 
 namespace Lesson
@@ -14,20 +14,47 @@ namespace Lesson
         {
             int[] myArray = null;
             return myArray; }
+        public class Person
+        {
+            public string FirstName { get; set; }
+
+            public string MiddleName { get; set; }
+
+            public string SecondName {  get; set; }
+
+            public Contacts Contacts { get; set; }
+            public string GetFullName() 
+            {
+                return  $"Фамилия:      {SecondName     ?? "null"} | " +
+                        $"Имя:          {FirstName      ?? "null"} | " +
+                        $"Отчество:     {MiddleName     ?? "null"}"; 
+            }
+
+            public string GetPhoneNumber() { return Contacts?.PhoneNumber ?? "null"; }
+        }
+        public class Contacts
+        {
+            public string PhoneNumber{get; set;}
+            public string Email { get; set;}
+        }
+        static Person GetPerson()
+        {
+            Person person = new Person() { Contacts=new Contacts() { PhoneNumber="123456789"} }; // null;
+            return person;
+        }
+
         static void Main(string[] args)
         {
-            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.Blue;
            
-            string str = null;
+           int[] myArr = GetArray();
 
-          str ??= string.Empty;
-            Console.WriteLine("Длинна строки = "+str.Length);
+          //  myArr ??= new int[0];
 
-            int[] myArr = GetArray();
-
-            myArr ??= new int[0];
-
-            Console.WriteLine("Колличество элементов в массиве = "+ myArr.Length);
+            Console.WriteLine("Сумма элементов в массиве = "+ (myArr?.Sum()??0));
+            Person person=GetPerson();
+            string phoneNumber = person?.Contacts?.PhoneNumber ?? "null";
+            Console.WriteLine(phoneNumber);
 
 
             
