3e06e3767903ffeead2674a96715490835d0fbed
CppKotlin <cscppkotlin@gmail.com>
2024-02-09 23:45:26 +0300

    //добавление элементов в массив

--- lessonCs/Program.cs
+++ lessonCs/Program.cs
@@ -4,50 +4,44 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-//изменение количества элементов в массиве
-// универсальный параметр T, универсальные шаблоны
-
+//добавление элементов в массив
 
 namespace Lesson
 {
     class Program
-    { static void Resize<T>(ref T [] array, int newSize)
+    { static void Insert<T>(ref T [] array,T value, int index)
         {
-            T[] newArray=new T[newSize];
-            for (int i=0; i<array.Length && i<newArray.Length; ++i)
+            if(index>array.Length) { index = array.Length; }
+            T[] newArray=new T[(array.Length+1)];
+            newArray[index]=value;
+            for (int i=0;  i<index; ++i)
             {
                 newArray[i]= array[i];
             }
-            array= newArray;
+            for (int i = index; i < array.Length; ++i)
+            {
+                newArray[i+1] = array[i];
+            }
+
+            array = newArray;
         }
         static void Main(string[] args)
         {
            
             int[] arr = { 4, 5, 2, 7, 9 };
+            foreach (var item in arr)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+            Insert(ref arr, -5, 20);
 
-            Array.Resize(ref arr, 8);
             foreach (var item in arr)
             {
                 Console.Write(item + " ");
             }
-            Console.WriteLine();
-            Resize(ref arr, 10);
-            foreach (var item in arr)
-            {
-                Console.Write(item+" ");
-            }
-            string[] strArr = { "oooo", "xxxxxx", "-------", "+++++++" };
-            Console.WriteLine();
-            foreach (var item in strArr)
-            {
-                Console.Write(item + " ");
-            }
-            Console.WriteLine();
-            Resize(ref strArr, 2);
-            foreach (var item in strArr)
-            {
-                Console.Write(item + " ");
-            }
+
+
 
 
             Console.ReadLine();
