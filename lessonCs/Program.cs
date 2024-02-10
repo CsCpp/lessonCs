aad7e808c869350d2eadc79620b24389b3826f54
CppKotlin <cscppkotlin@gmail.com>
2024-02-10 23:25:42 +0300

    //удаление элементов массива

--- lessonCs/Program.cs
+++ lessonCs/Program.cs
@@ -4,63 +4,50 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-//добавление элементов в начало и конец массива
+//удаление элементов массива
 
 namespace Lesson
 {
     class Program
-    {   static void Insert<T>(ref T [] array,T value, int index)
+    {  
+        static void PrintArr(ref int[] arr)
         {
-            if(index>array.Length) { index = array.Length; }
-            T[] newArray=new T[(array.Length+1)];
-            newArray[index]=value;
-            for (int i=0;  i<index; ++i)
-            {
-                newArray[i]= array[i];
-            }
-            for (int i = index; i < array.Length; ++i)
-            {
-                newArray[i+1] = array[i];
-            }
+            for (int i = 0; i < arr.Length; i++)
+                Console.Write(arr[i] + " ");
+            Console.WriteLine();
+        }
+        static void RemoveAt (ref int[] array, int index)
+        {
+            if(index>(array.Length-1)) index = array.Length-1;
 
-            array = newArray;
+            int[] newArr = new int[array.Length - 1];
+            for(int i=0; i<index; i++)
+               newArr[i] = array[i];
+            for(int i=index+1; i<array.Length; i++)
+                newArr[i-1] = array[i];
+            array=newArr;
         }
-        static void AddFirst<T>(ref T[] array, T valuex)
+        static void RemoveFirst(ref int[] array)
         {
-            Insert(ref array, valuex, 0);
+            RemoveAt(ref array, 0);
         }
-        static void AddLast<T>(ref T[] array, T valuex)
+        static void RemoveLast(ref int[] array)
         {
-            Insert(ref array, valuex,array.Length);
+            RemoveAt(ref array, array.Length-1);
         }
+
         static void Main(string[] args)
         {
            
             int[] arr = { 4, 5, 2, 7, 9 };
-            foreach (var item in arr)
-            {
-                Console.Write(item + " ");
-            }
-            Console.WriteLine();
-            Insert(ref arr, -5, 20);
+           PrintArr(ref arr);
 
-            foreach (var item in arr)
-            {
-                Console.Write(item + " ");
-            }
-            Console.WriteLine();
-            AddFirst(ref  arr, 78);
-            foreach (var item in arr)
-            {
-                Console.Write(item + " ");
-            }
-            Console.WriteLine();
-            AddLast(ref arr, -999);
-            foreach (var item in arr)
-            {
-                Console.Write(item + " ");
-            }
-            Console.WriteLine();
+            RemoveAt(ref arr, 3);
+            PrintArr(ref arr);
+            RemoveFirst(ref arr);
+            PrintArr(ref arr);
+            RemoveLast(ref arr);
+            PrintArr(ref arr);
 
             Console.ReadLine();
         }
