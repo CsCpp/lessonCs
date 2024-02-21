b748e24ac34b38aeeefb23b955d2a4ed574de91e
CppKotlin <cscppkotlin@gmail.com>
2024-02-21 23:08:41 +0300

    "Ядерный Ганди" и арифметическое переполнение

--- lessonCs/Program.cs
+++ lessonCs/Program.cs
@@ -4,7 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-//         Преобразование и приведение примитивных типов
+//         "Ядерный Ганди" и арифметическое переполнение
+
 
 
 
@@ -13,24 +14,26 @@ namespace Lesson
     class Program
     {
 
-        static void Foo (float value)
-        {
-            Console.WriteLine(value);
-         
-        }
-       
         static void Main(string[] args)
         {
-            int a = 258;
-            byte b =(byte) a;  // byte - MAX - 255
-            Foo(a);
-            Console.WriteLine(b);
-            bool c=Convert.ToBoolean(a);
-            Console.WriteLine(c);
-            float e=2.6F;
-            int result = a + (int)e; //(int)(a+e);
-            Console.WriteLine(result);
-            
+            byte agression = 1;
+            byte democracyModifier = 2;
+            agression=(byte)(agression-democracyModifier);
+
+            Console.WriteLine(agression);
+           
+            checked
+            {
+                democracyModifier -= 2;
+            }
+
+            double s = 1.0 / 0.0;
+            Console.WriteLine(double.IsInfinity(s));
+
+
+
+
+
             Console.ReadLine();
         }
 
