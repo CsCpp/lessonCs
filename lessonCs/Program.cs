55dc4603e75b2093247064af1941f41fa372990b
CppKotlin <cscppkotlin@gmail.com>
2024-03-07 22:19:44 +0300

    ключевое слово static

--- lessonCs/Program.cs
+++ lessonCs/Program.cs
@@ -7,62 +7,30 @@ using System.Text;
 using System.Threading.Tasks;
 
 //      OOП
-//   Автоматические свойства
+//   ключевое слово static
 
 namespace Lesson
 {
     class Point
     {
-        public Point(int x, int y)
-        {
-            this._x = x;
-            _y = y;
-        }
-        public Point(int x, int y, int z) : this(x, y)
-        {
-            _z = z;
-        }
-
-       
-
-        public int _Y
-        {
-            get { return _y; }
-            set {
-                if (value < 1)
-                {
-                    _y = 1;
-                    return;
-                }
-                if(value>5)
-                {
-                    _y = 5;
-                    return;
-                }
-                _y = value; }
-        }
-
-
-
-        public int _L { get; set; }
-
-        private int _x;
-        private int _y;
-        private int _z;
+        public int a;
+        public static int b;
     }
+       
     class Program
     {
        
         static void Main(string[] args)
         {
 
-            Point point = new Point(2, 3, 6);
+            Point point = new Point();
+                point.a = 22;
+                Point point2 = new Point();
+                point2.a = 33;
+                Point.b = 1;
 
-            point._Y = 20;
-            int rrr= point._Y;
-            point._L = rrr;
-            rrr= point._L;
-            Console.ReadLine();
+
+                Console.ReadLine();
         }
 
         
