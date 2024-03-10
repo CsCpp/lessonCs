c4146f9e5c38c76fe6006ccdd75da76f819ce82a
CppKotlin <cscppkotlin@gmail.com>
2024-03-10 22:51:35 +0300

    //   static конструктор

--- lessonCs/Program.cs
+++ lessonCs/Program.cs
@@ -7,57 +7,34 @@ using System.Text;
 using System.Threading.Tasks;
 
 //      OOП
-//   static свойства
+//   static конструктор
 
 namespace Lesson
 {
-    class Point
+    class MyClass
     {
-        public Point()
+        public MyClass() 
         {
-            Counter++;
+            Console.WriteLine("MyClass  конструктор");
         }
-        
-        private static int a;
-
-        public static int A
+        static MyClass()
         {
-            get { return a; }
-            set { a = value; }
+            Console.WriteLine("static MyClass  конструктор");
         }
-        public static int B { get; set; }
-
-        private static int counter;
-
-        public int Counter
+        public static void Foo()
         {
-            get { return counter; }
-           private set { counter = value; }
+            Console.WriteLine("static Foo");
         }
-
-        public int getCounter() 
-        {
-            return Counter;
-        }
-
     }
        
     class Program
-    {
-       
+    {       
         static void Main(string[] args)
         {
-            Point point = new Point();
-            Point.A = 23;
-            int b=Point.A;
-            int c=Point.A;
-            Point point2 = new Point();
-            Point point3 = new Point();
-           // Point point4 = new Point();
-           
-            Console.WriteLine(point.getCounter());
-            Console.WriteLine(point3.getCounter());
-            Console.WriteLine(point2.Counter);
+           MyClass myClass = new MyClass();
+            MyClass myClass2 = new MyClass();
+            MyClass myClass3 = new MyClass();
+
 
             Console.ReadLine();
         }
