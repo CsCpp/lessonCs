9a62776932c75729b8745b553b37786e6451037e
CppKotlin <cscppkotlin@gmail.com>
2024-03-18 22:16:37 +0300

    // const VS readonly

--- lessonCs/Program.cs
+++ lessonCs/Program.cs
@@ -7,20 +7,35 @@ using System.Text;
 using System.Threading.Tasks;
 
 //      OOП
-//  частичные типы // partial  классы, методы
+// const VS readonly
 namespace Lesson
 {
+    class MyClass
+    {
+        
+        public const double PI=3.14;
+        public const string MY_ERROR = "some error";
+        public readonly int a;
+        public MyClass(int _a = 3) 
+        {
+        a=_a;
+        }
+        public void Foo() 
+        {
+           
+        }
+    }
     
     class Program
     {       
         static void Main(string[] args)
         {
 
-            Person person = new Person("Linkoln", "Pavel");
-            person.PrintFullName();
 
+            Console.WriteLine(MyClass.MY_ERROR);
+            MyClass myClass = new MyClass(44);
 
-
+            Console.WriteLine(myClass.a);
 
             Console.ReadLine();
         }
