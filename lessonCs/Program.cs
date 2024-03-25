7066b82a82aabeab2a71114c59ccff776df9f2db
CppKotlin <cscppkotlin@gmail.com>
2024-03-25 23:13:11 +0300

    полтморфиз // virtual // override

--- lessonCs/Program.cs
+++ lessonCs/Program.cs
@@ -7,47 +7,51 @@ using System.Text;
 using System.Threading.Tasks;
 
 //      OOП
-// protected и наследование
+// полтморфиз
+// virtual
+// override
 namespace Lesson
 {
-    class A
+    class Car
     {
-        public int publicFiled;
-        private int privateFiled;
-        protected int protectedFiled;
-
-        public A()
+        protected virtual void StartEngine()
         {
-            Console.WriteLine(publicFiled);     //поле доступно
-            Console.WriteLine(privateFiled);    //поле доступно
-            Console.WriteLine(protectedFiled);  //поле доступно
+            Console.WriteLine("Двигатель работает!");
         }
-        public void Foo()
+        public virtual void Drive()
         {
-            Console.WriteLine(publicFiled);     //поле доступно
-            Console.WriteLine(privateFiled);    //поле доступно
-            Console.WriteLine(protectedFiled);  //поле доступно
+            StartEngine();
+            Console.WriteLine("Машинка  чухчухчух");
         }
     }
-    class B : A
+    class SportCar : Car
     {
-        public B()
+        protected override void StartEngine()
         {
-            Console.WriteLine(publicFiled);     //поле доступно
-                                                //  Console.WriteLine(privateFiled);    //поле НЕдоступно
-            Console.WriteLine(protectedFiled);  //поле доступно
+            Console.WriteLine("Двигатель, ВАСССЯ, РРРРРНДНДН!");
+        }
+        public override void Drive()
+        {
+            StartEngine();
+            Console.WriteLine("Машинка быстро бЗЗЗЗЗЗ");
         }
     }
+    class Person
+    {
+        public void Drive(Car car)
+        {
+            
+            car.Drive();
+        }
+    }
+   
     class Program
     {       
         static void Main(string[] args)
         {
-            A a = new A();
-            a.Foo();    
-
-                B b = new B();
-                b.Foo();
-            
+           Person person = new Person();
+            person.Drive(new Car());
+            person.Drive(new SportCar());
 
 
             Console.ReadLine();
