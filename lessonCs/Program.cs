06c5349998515b12b8136d5d568a1c605d5a705d
CppKotlin <cscppkotlin@gmail.com>
2024-03-28 22:47:19 +0300

    // полтморфизм и интерфейс

--- lessonCs/Program.cs
+++ lessonCs/Program.cs
@@ -7,66 +7,58 @@ using System.Text;
 using System.Threading.Tasks;
 
 //      OOП
-// полтморфизм
-// обстрактный класс, метод, свойство
+// полтморфизм и интерфейс
+
 
 
 namespace Lesson
 {
-    abstract class Weapon
+    interface IDateProvider
     {
-        public abstract void Fire();
-        public abstract int Damage { get; }
-
-        public void ShowInfo()
-        {
-            Console.WriteLine($"{GetType().Name} Damage: {Damage}");
-
-        }
-        
+        string GetData();
     }
-   class Player
+    interface IDataProcessor
     {
-        public void Fire(Weapon weapon)
+        void ProcessData(IDateProvider dateProvider);
+    }
+    class ConsoleDataProcessor : IDataProcessor
+    {
+        public void ProcessData(IDateProvider dateProvider)
         {
-            weapon.Fire();
-        }
-        public void ShowInfo(Weapon weapon)
-        {
-        weapon.ShowInfo();  
+            Console.WriteLine(dateProvider.GetData());
         }
     }
-    class Gun : Weapon
+    class DbDataProvider : IDateProvider
     {
-        public override int Damage { get { return 23; } }
-
-        public override void Fire()
+        public string GetData()
         {
-            Console.WriteLine("BUM!");
+            return "Данные из базы данных";
         }
     }
-    class Bow : Weapon
+    class FileDataProvider : IDateProvider
     {
-        public override int Damage => 3;
-        public override void Fire()
+        public string GetData()
         {
-            Console.WriteLine("Fiuxxx!");
+            return "Данные из файла";
+
         }
     }
-
+    class APIDataProvider : IDateProvider
+    {
+        public string GetData()
+        {
+            return "Данные из API";
+        }
+    }
     class Program
     {       
         static void Main(string[] args)
         {
-           Player player = new Player();
-            Weapon[] inwentory = {new Gun(),new Bow()};
-            foreach (var iter in inwentory) 
-            {
-                player.ShowInfo(iter);
-                player.Fire(iter);
-                Console.WriteLine();
-            }
-            
+           IDataProcessor processor = new ConsoleDataProcessor();   
+
+            processor.ProcessData(new APIDataProvider());
+            processor.ProcessData(new FileDataProvider());
+            processor.ProcessData(new DbDataProvider());
 
 
 
