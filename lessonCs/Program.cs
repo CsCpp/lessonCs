8c57fb1c323535e24964fe45130c3d55ddd7dd33
CppKotlin <cscppkotlin@gmail.com>
2024-04-02 23:01:45 +0300

    // структуры

--- lessonCs/Program.cs
+++ lessonCs/Program.cs
@@ -7,20 +7,35 @@ using System.Text;
 using System.Threading.Tasks;
 
 //      OOП
-//  реализация интерфейса по умолчанию
+// структуры
 
 
 namespace Lesson
 {
+    class ClassPoint
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public void Print()
+        {
+            Console.WriteLine($"X:{X}, Y:{Y}");
+        }
+    }
+    struct StructPoint
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public void Print()
+        {
+            Console.WriteLine($"X:{X}, Y:{Y}");
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            ILogger consoleLoger = new ConsoleLogger();
-            consoleLoger.Log(LogLavel.Debug, "some event");
-            consoleLoger.Log(LogLavel.Warning, "some WARNING");
-            consoleLoger.Log(LogLavel.Fatal, "some Fatal ERROR");
-            consoleLoger.Foo();
+            
+
             Console.ReadLine();
         }
            
